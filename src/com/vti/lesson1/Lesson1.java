3dad4dc318071bd3ce82daba1f9bc609767b7f81
nguyenanhdung <mrad9x@gmail.com>
2023-11-04 20:43:36 +0700

    dungna31
    add file

---
+++ src/com/vti/lesson1/Lesson1.java
@@ -0,0 +1,11 @@
+package com.vti.lesson1;
+
+/**
+ * @created: 04/11/2023 - 8:33 PM
+ * @author: dungna
+ */
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
